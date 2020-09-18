6ef670f7d8054aa444831188d3ef52464ba45e3c
Danilo Salvador <danilosalvador@Mac-mini-de-Danilo.local>
2020-09-18 00:20:33 -0300

    Input e Button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Title } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import imgLogo from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={imgLogo} />
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        style={{ marginTop: 16 }}
+        onPress={() => {
+          console.log('Entrar');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
